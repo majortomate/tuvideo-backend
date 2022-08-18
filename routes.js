b5fa205c0d50668615718a163e17cf99b745d165
Carlos <mac@Majortomate.local>
2022-08-18 11:34:29 -0500

    feat: add upload endpoint with cloudinary working

--- routes.js
+++ routes.js
@@ -5,11 +5,13 @@ const user = require('./api/user/index.js')
 const video = require('./api/video/index.js')
 const channel = require('./api/channel/index.js')
 const admin = require('./api/admin/index.js')
+const upload = require('./api/upload/index.js')
 const routes = function (app) {
   app.use('/api/users', user)
   app.use('/api/videos', video)
   app.use('/api/channel', channel)
   app.use('/api/admin', admin)
+  app.use('/api/upload', upload)
 }
 
 module.exports = routes
